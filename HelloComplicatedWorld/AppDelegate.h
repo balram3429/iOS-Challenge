dbe80fbb6a1b083c4bbcdd38b833e81c7ef28296
Nathan <nathan@janrain.com>
2013-05-08 12:19:37 -0700

    Initial Commit

---
+++ HelloComplicatedWorld/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HelloComplicatedWorld
+//
+//  Created by Nathan on 5/8/13.
+//  Copyright (c) 2013 Janrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ HelloComplicatedWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloComplicatedWorld
+//
+//  Created by Nathan on 5/8/13.
+//  Copyright (c) 2013 Janrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
